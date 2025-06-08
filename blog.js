05e8f2eeda4123af3a780a54390750584af169ab
aweda02 <awedaabdullahi35@gmail.com>
2025-06-08 17:06:57 +0100

    Add files via upload

--- blog.js
+++ blog.js
@@ -22,7 +22,28 @@ if(scrollPos > 80){
  }
 });
 
-
-
-
 // end of toggle btn
+
+
+var blogContent = document.querySelectorAll('.blog-content'); 
+blogContent.forEach(function(item){
+  item.addEventListener('click',function(e){ 
+if('click'){
+var currenttarget = e.currentTarget;
+currenttarget.classList.toggle('active');
+}else{
+  item.classList.remove('active')
+}
+// toggle hidden text
+let hiddenArticle = currenttarget.querySelector(".hidden-article")
+var moretext = currenttarget.querySelectorAll('.more-text');
+moretext.forEach(function(item){item.classList.toggle('more-none')})
+  if(hiddenArticle){
+    hiddenArticle.classList.toggle('active-text');
+  }
+let contentimg = currenttarget.querySelector(".content-img")
+  if(hiddenArticle){
+    contentimg.classList.toggle('active-img');
+  }
+  })
+}) 
